d54975a16bfa456c565b9633e017dc1fdf2d56dd
churakovra <churakov.main@gmail.com>
2025-04-12 19:05:54 +0300

    Add get wallet data functionality

--- app/controllers/TronController.py
+++ app/controllers/TronController.py
@@ -1,9 +1,37 @@
 from tronpy import Tron
+from tronpy.providers import HTTPProvider
 
+from app.config.preferences import API_KEY
 from app.models.WalletModel import Wallet
+from app.models.WalletResponseModel import WalletResponse
 
-client = Tron()
+client = Tron(
+    provider=HTTPProvider(
+        api_key=API_KEY
+    )
+)
 
 
-async def get_wallet_info(wallet: Wallet):
-    pass
+async def get_wallet_data_from_tron(wallet: Wallet) -> WalletResponse:
+    bandwidth = client.get_bandwidth(wallet.address)
+    account_balance = client.get_account_balance(wallet.address)
+    account_energy = await get_account_energy(wallet)
+    response = WalletResponse(
+        wallet_addr=wallet.address,
+        bandwidth=bandwidth,
+        account_energy=account_energy,
+        account_balance=account_balance
+    )
+    return response
+
+
+async def get_account_energy(addr) -> int:
+    account_frozen_v2 = client.get_account(addr)["frozenV2"]
+    energy_weight = account_frozen_v2["amount"]
+
+    account_resources = client.get_account_resource(addr)
+    total_energy_limit = account_resources["TotalEnergyLimit"]
+    total_energy_weight = account_resources["TotalEnergyWeight"]
+
+    account_energy = int((energy_weight / total_energy_weight) * total_energy_limit)
+    return account_energy
