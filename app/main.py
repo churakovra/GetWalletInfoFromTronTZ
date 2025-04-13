8e01206cfd11b5a8c204169623b38b072fcd340c
churakovra <churakov.main@gmail.com>
2025-04-13 15:32:47 +0300

    add root controller

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 
 from app.controllers.GetQueryHistoryController import get_query_history_router
 from app.controllers.GetWalletDataController import get_wallet_data_router
-from app.database import SessionLocal
+from app.controllers.RootController import root_router
 
 app = FastAPI()
 
+app.include_router(root_router)
 app.include_router(get_wallet_data_router)
 app.include_router(get_query_history_router)
