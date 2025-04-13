5f735c40702269227ebdabc5a2642aedff830684
churakovra <churakov.main@gmail.com>
2025-04-13 17:53:39 +0300

    add autocreating tables

--- app/database.py
+++ app/database.py
@@ -1,10 +1,16 @@
+from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.config.preferences import DATABASE_URL
+from app.models.WalletLogModelDB import Base
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+async def lifespan_init_db(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
 
 def get_db():
     db = SessionLocal()
