85664f9462d7d913f6e617a24346bcc03dbac0fa
churakovra <churakov.main@gmail.com>
2025-04-13 17:53:22 +0300

    add exception handling in post endpoint

--- app/controllers/LogWalletDBController.py
+++ app/controllers/LogWalletDBController.py
@@ -2,18 +2,21 @@ from datetime import datetime
 
 from sqlalchemy.orm import Session
 
-from app.models.WalletLogModelDB import WalletLog
+from app.models.WalletLogModelDB import WalletLogDB
 from app.models.WalletModel import Wallet
 
 
-async def wallet_db_log(wallet: Wallet, user_host: str, db: Session):
+async def wallet_db_log(wallet: Wallet, user_host: str, db: Session) -> bool:
     current_dt = datetime.now()
-    wallet_log = WalletLog(
+    wallet_log = WalletLogDB(
         wallet_address=wallet.address,
         query_datetime=current_dt,
         query_user=user_host
     )
-    db.add(wallet_log)
-    db.commit()
-    db.refresh(wallet_log)
-    return wallet_log
+    try:
+        db.add(wallet_log)
+        db.commit()
+        db.refresh(wallet_log)
+    except Exception:
+        return False
+    return True
